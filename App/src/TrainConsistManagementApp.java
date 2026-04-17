08c8392f1309d74d342e921ddbf1a63ac30289fe
ak0215-ux <ak0215@srmist.edu.in>
2026-04-17 20:06:22 +0530

    Add files via upload

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,28 +1,43 @@
 import java.util.*;
-public class TrainConsistManagementApp{
-    public static void main(String[] args){
-        System.out.println("==========================================");
-        System.out.println("UC20 - Exception Handling During Search");
-        System.out.println("==========================================");
-        System.out.println();
-        String[] bogieIds = {};
-        String searchId = "BG101";
-        if (bogieIds.length == 0){
-            throw new IllegalStateException("No bogies available in train. Cannot perform search.");
+
+public class TrainConsistManagementApp {
+    static class InvalidCapacityException extends Exception{
+        public InvalidCapacityException(String message){
+            super(message);
         }
-        boolean found = false;
-        for (String id : bogieIds){
-            if (id.equals(searchId)){
-                found = true;
-                break;
+    }
+    static class Bogie {
+        String type;
+        int capacity;
+
+        Bogie(String type, int capacity) throws InvalidCapacityException {
+            if (capacity <= 0){
+                throw new InvalidCapacityException("Capacity must be greater than zero.");
             }
+            this.type = type;
+            this.capacity = capacity;
         }
-        if (found){
-            System.out.println("\nBogie " +searchId+ " found using search operation.");
+    }
+    public static void main(String[] args) throws InvalidCapacityException{
+        System.out.println("=========================================");
+        System.out.println("UC14 - Handle Invalid Bogie Capacity ");
+        System.out.println("=========================================");
+
+        List<Bogie> bogies = new ArrayList<>();
+        try{
+            Bogie b1 = new Bogie("Sleeper",72);
+            bogies.add(b1);
+            System.out.println("\nCreated Bogie: " +b1.type+ " -> " +b1.capacity);
+        }catch (InvalidCapacityException e){
+            System.out.println("Error: " +e.getMessage());
         }
-        else{
-            System.out.println("\nBogie " +searchId+ " not found using search operation.");
+        try{
+            Bogie b2 = new Bogie("Sleeper", -10);
+            bogies.add(b2);
+            System.out.println("Created Bogie: " +b2.type+ " -> " +b2.capacity);
+        }catch (InvalidCapacityException e){
+            System.out.println("Error: " +e.getMessage());
         }
-        System.out.println("\nUC20 execution completed...");
+        System.out.println("\nUC14 exception handling completed...");
     }
 }
